4ae2519ae4db76b469574679e532534ec073bdad
jmf0991 <jmf0991@gmail.com>
2019-07-19 14:58:24 +0200

    added classes to models folder

---
+++ CustomAuthenticationMVC/Models/RegistrationView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomAuthenticationMVC.Models
+{
+    public class RegistrationView
+    {
+    }
+}
