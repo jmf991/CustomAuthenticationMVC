b7993e883089388c5e25695f86900c7016b00898
jmf0991 <jmf0991@gmail.com>
2019-07-19 14:43:28 +0200

    added account corntroller, custom principal, custom role

--- CustomAuthenticationMVC/Global.asax.cs
+++ CustomAuthenticationMVC/Global.asax.cs
@@ -1,9 +1,12 @@
+using CustomAuthenticationMVC.CustomAuthentication;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace CustomAuthenticationMVC
 {
@@ -13,6 +16,26 @@ namespace CustomAuthenticationMVC
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies["Cookie1"];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                var serializeModel = JsonConvert.DeserializeObject<CustomSerializeModel>(authTicket.UserData);
+
+                CustomPrincipal principal = new CustomPrincipal(authTicket.Name);
+
+                principal.UserId = serializeModel.UserId;
+                principal.FirstName = serializeModel.FirstName;
+                principal.LastName = serializeModel.LastName;
+                principal.Roles = serializeModel.RoleName.ToArray<string>();
+
+                HttpContext.Current.User = principal;
+            }
+
         }
     }
 }
